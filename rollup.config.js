2555401eadebaf46f78870453d56e1df11aa0365
shallinta <740159135@qq.com>
2021-05-28 16:50:46 +0800

    feat: revert changelog cli

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+export default [{
+  input: 'src/index.js',
+  output: [{
+    file: 'dist/index.js',
+    format: 'cjs',
+  }],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    terser({
+      keep_fnames: true,
+    }),
+  ],
+}];
---
+++ src/index.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+let [_1, _2, versionStr, filename] = process.argv;
+
+const versionRegEx = /^[vV@#]?(\d+\.\d+\.\d+)$/;
+
+if (versionStr) {
+  if (versionRegEx.test(versionStr)) {
+    versionStr = versionStr.match(versionRegEx)[1];
+  } else if (!filename) {
+    filename = versionStr;
+    versionStr = '';
+  } else {
+    versionStr = '';
+  }
+}
+
+if (!filename) {
+  filename = 'CHANGELOG.md';
+}
+
+if (fs.existsSync(filename)) {
+  const changelog = fs.readFileSync(filename, 'utf-8');
+  const EOF = '## 0.0.0';
+  const changelogForReg = changelog + EOF;
+
+  const logBlockRegEx = /#+ \d+\.\d+\.\d+([\s\S\n]*?)(?=#+ \d+\.\d+\.\d+)/g;
+  const logBlockVerRegEx = /(?<=#+ )\d+\.\d+\.\d+/;
+
+  const logBlocks = changelogForReg.match(logBlockRegEx);
+
+  if (logBlocks) {
+    const logBlocksData = logBlocks.map((blockStr) => {
+      const blockVer = blockStr.match(logBlockVerRegEx)[0];
+      return { ver: blockVer, log: blockStr };
+    });
+
+    if (!versionStr) {
+      versionStr = logBlocksData[0].ver;
+    }
+
+    const revertIndex = logBlocksData.findIndex(({ ver }) => ver === versionStr);
+    if (revertIndex > -1) {
+      const revertLogBlocks = logBlocks.slice(0, revertIndex + 1);
+      const revertedChangelog = changelog.replace(revertLogBlocks.join(''), '');
+      fs.writeFileSync(filename, revertedChangelog);
+    }
+  }
+}
